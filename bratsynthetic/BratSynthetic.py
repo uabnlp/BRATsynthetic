c81529a45cc5d883116d64e1fc49211caa0bedad
Chris Coffee <chrico@uab.edu>
2024-07-08 18:29:01 -0500

    running offset for spans

--- bratsynthetic/BratSynthetic.py
+++ bratsynthetic/BratSynthetic.py
@@ -81,7 +81,9 @@ class BratSynthetic:
             assert type(annotation) == BratEntity
             current_entity: BratEntity = annotation
             replacement_text = replacements[current_entity]
-            if current_entity.text == replacement_text:
+            original_text = current_entity.text
+            if original_text == replacement_text:
+                print(f"WARNING: {current_entity} -> {replacement_text}")
                 new_brat_annotations.append(annotation)
                 continue
             # else annotation in replacements with new text
@@ -91,13 +93,13 @@ class BratSynthetic:
                 print(f"[WARNING]: {current_entity} -> {replacement_text}")
                 print("[WARNING][WARNING][WARNING][WARNING][WARNING][WARNING][WARNING][WARNING][WARNING][WARNING]")
 
-            new_text = new_text[:current_entity.start() - offset] + replacement_text + new_text[current_entity.end() - offset:]
-            current_entity.spans = [(current_entity.start() - offset, current_entity.start() - offset + len(replacement_text))]
+            new_text = new_text[:current_entity.start() + offset] + replacement_text + new_text[current_entity.end() + offset:]
+            current_entity.spans = [(current_entity.start() + offset, current_entity.start() + offset + len(replacement_text))]
             current_entity.text = self.get_brat_text_from_spans(new_text, current_entity.spans)
             print(f'{brat_file.identifier_to_annotation[current_entity.identifier]} -> {current_entity}')
             new_brat_annotations.append(current_entity)
 
-            offset += len(replacement_text) - len(current_entity.text)
+            offset += len(replacement_text) - len(original_text)
 
         new_brat_file = BratFile(new_text, new_brat_annotations)
 
