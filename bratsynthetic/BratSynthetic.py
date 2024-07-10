4c1e8f1acf10626b4df51e7362c27eed2e33b8f1
Chris Coffee <chrico@uab.edu>
2024-07-09 23:10:24 -0500

    slight adjustment 3 to previous fix

--- bratsynthetic/BratSynthetic.py
+++ bratsynthetic/BratSynthetic.py
@@ -75,8 +75,7 @@ class BratSynthetic:
         offset = 0
         for index, annotation in enumerate(new_annotations):
             if annotation.text[:2] == ", ":
-                leading_text = ", " if "," not in replacements[annotation] else " "
-                replacements[annotation] = leading_text + replacements[annotation].replace(",", "")
+                replacements[annotation] = ", " + replacements[annotation].replace(",", "")
             if annotation not in replacements or annotation.text == ",":
                 if annotation.text == ",":
                     print(f'Skipping comma: {annotation}')
