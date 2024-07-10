810709fda62ad4deff098d70bc4de45e691d2970
Chris Coffee <chrico@uab.edu>
2024-07-09 23:00:02 -0500

    fix to missing leading comma+space

--- bratsynthetic/BratSynthetic.py
+++ bratsynthetic/BratSynthetic.py
@@ -74,6 +74,8 @@ class BratSynthetic:
         new_brat_annotations: List[BratEntity] = []
         offset = 0
         for index, annotation in enumerate(new_annotations):
+            if annotation.text[:2] == ", ":
+                replacements[annotation] = ", " + replacements[annotation]
             if annotation not in replacements or annotation.text == ",":
                 if annotation.text == ",":
                     print(f'Skipping comma: {annotation}')
